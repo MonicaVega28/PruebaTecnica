dbb68675db380d9f5a2d9d72f5aeecac46081550
MonicaVega28 <33848220+MonicaVega28@users.noreply.github.com>
2021-09-13 15:20:35 -0300

    Add files via upload

---
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterface/ChoucairAcademyPage.java
@@ -0,0 +1,12 @@
+package co.com.choucair.certification.proyectobase.userinterface;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class ChoucairAcademyPage extends PageObject {
+
+    public static ChoucairAcademyPage extends PageObject{
+        public static final Target LOGIN_BUTTON = Target.the("button that shows us the form to login").located(By.xpath(//div[@class=id-none'd-lg-block']//strong[contains(text(),'Ingresar)]);
+    }
+}
